39bc3590ba2161bf38b8e0f796bb34789fd9e637
Fenil-1 <fenilp2046@gmail.com>
2025-07-29 23:14:20 +0530

    new quotes

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,16 @@ import XIcon from '@mui/icons-material/X';
 function App() {
    const arr = [
     {
-      quote : 'yo1' , 
-      author : 'aut 1'
+      quote : "It's gonna be legen—wait for it—dary!" , 
+      author : 'Barney Stinson'
     },
     {
-      quote : 'yo2' , 
-      author : 'aut 2'
+      quote : "I don’t trust banks. I believe that when the robots rise up, ATMs will lead the charge.", 
+      author : 'Sheldon'
     },
     {
-      quote : 'yo3' , 
-      author : 'aut 3'
+      quote : 'I fear not the man who has practiced 10,000 kicks once, but I fear the man who has practiced one kick 10,000 times.' , 
+      author : 'Bruce Lee'
     }
   ]
 
